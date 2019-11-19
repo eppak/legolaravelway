9eabc230a20736759529e0351e84480f7e908b2e
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2019-11-19 03:28:41 -0500

    Init backpack

---
+++ server/app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+
+use Agronomist\Models\Category as Model;
+
+class Category extends Model 
+{
+    use CrudTrait;
+
+}
---
+++ server/app/Models/Vitamin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+
+use Agronomist\Models\Vitamin as Model;
+
+class Vitamin extends Model
+{
+    use CrudTrait;
+}
